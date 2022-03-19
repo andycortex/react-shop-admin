cf93f2c4477987dc5ce0419b054021a41615fcec
Cortex <cortex.andy2809@gmail.com>
2022-03-18 21:25:48 -0400

    desarollo de dashboard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css';
+import { ProviderAuth } from '@hooks/useAuth';
+import MainLayout from '@layouts/MainLayout';
+import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // return <Component {...pageProps} />;
+  return(
+    <ProviderAuth>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ProviderAuth>
+  );
 }
 
 export default MyApp;
